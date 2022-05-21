5b6d5e7125c11a15bd5175ee62283b0cf11d9b16
akshyv <akshy.nattu@gmail.com>
2022-05-21 15:15:04 +0530

    added heroes list and selecting property

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { HEROES } from '../mocks/mock'
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl:'./app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Tour of Heroes';
+  heroes = HEROES;
+  selectedHero? : Hero
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
 }
